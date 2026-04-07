2a35bb4fa29dbfc823ea6969602a4d38234c5197
gsklei <gklein8@lsu.edu>
2026-04-07 00:46:59 +0000

    completed TypeAnnotationPass

--- prog3_skeleton_code/prog3_skeleton_code/Typecheck/Pass/TypeAnnotationPass.java
+++ prog3_skeleton_code/prog3_skeleton_code/Typecheck/Pass/TypeAnnotationPass.java
@@ -5,6 +5,7 @@ import java.util.stream.*;
 import Typecheck.TypeCheckException;
 
 public class TypeAnnotationPass extends Pass<Void> {
+   
 
     // Hint: Build the base type from the name, then wrap it for pointers and any [] modifiers.
     // 1. Construct the base type ("string" -> STRING)
@@ -43,7 +44,34 @@ public class TypeAnnotationPass extends Pass<Void> {
                   node.name.equals("string") ? new STRING() :
                   node.name.equals("void") ? new VOID() :
                   new ALIAS(node.name);
-
+               
+      for (int i = 0; i < node.pointerCount; i++) {
+         basetype = new POINTER(basetype);
+      }
+      if (node.brackets.list.size() == 0) {
+         node.typeAnnotation = basetype;
+         return null;
+      }
+      if (isLIST) {
+         Type current = basetype;
+         for (int i = 0; i < node.brackets.list.size(); i++) {
+            Absyn.ArrayType at = (Absyn.ArrayType) node.brackets.list.get(i);
+            int size = ((Absyn.DecLit) at.size).value;
+            ArrayList<Type> types = new ArrayList<>();
+            for (int j = 0; j < size; j++) {
+               types.add (current);
+            }
+            current = new LIST(types);
+         }
+         node.typeAnnotation = current;
+      }
+      else if (isARRAY && node.brackets.list.size() > 0) {
+         Type current = basetype;
+         for (int i = 0; i < node.brackets.list.size(); i++) {
+            current = new ARRAY(current);
+         }
+         node.typeAnnotation = current;
+      }
                   return null;
 
    }
